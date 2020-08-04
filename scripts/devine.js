8de9c1f75d9300390e3dab2bbe45dfb51e877330
jacquadit7@gmail.com <jacquadit7@gmail.com>
2020-08-04 09:24:01 +0200

    mise au propre de devine.js

--- scripts/devine.js
+++ scripts/devine.js
@@ -1,12 +1,3 @@
-//var lettre = 'v';
 var reponse = prompt("devine une lettre : ");
 
-//	if (reponse === null){
-//		break;
-//	}
-//else if(reponse === lettre){
-//	alert("bravo c'est bien la lettre "+lettre);
-//}
-//else {}
-//if (reponse === lettre){
 	alert("bonne lettre "+ reponse);
