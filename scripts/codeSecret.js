65384b6d5e416cd164f13bf41ce00d13c5b2c433
jacquadit7@gmail.com <jacquadit7@gmail.com>
2020-08-06 11:01:10 +0200

    correction des if else

--- scripts/codeSecret.js
+++ scripts/codeSecret.js
@@ -1,3 +1,17 @@
+var motSecret="pfmp";
+var annee=2020;
+
 var motEntree = prompt("Entrez le mot secret ");
-alert("votre mot est bien "+ motEntree);
+var anneeEntree = prompt("entrez l'annee : ");
+alert("votre mot est bien "+ motEntree + "  l'année est bien "+anneeEntree);
+
+if(motEntree === motSecret){
+	alert("C'est deja le bon mot secret "+motSecret);
+}		
+	else if(anneeEntree===annee){
+	alert("c'est la bonne anne : " + annee);
+	}			
+	else 
+	{alert("non ce n'est pas le bon mot secret, vous avez entré :"+motEntree);
+	}
 
